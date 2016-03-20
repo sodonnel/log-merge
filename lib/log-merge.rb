506e4f1f4537747ff102f2a55593c1b0480816f3
Stephen ODonnell <sodonnell@cloudera.com>
2016-03-20 22:28:40 +0000

    Added very simple CLI class to get a basic command line merge tool working

--- lib/log-merge.rb
+++ lib/log-merge.rb
@@ -1,8 +1,10 @@
 require 'date'
+require 'ostruct'
+require 'optparse'
 
 require 'log-merge/log_reader'
 require 'log-merge/log_line_builder'
 require 'log-merge/log_line'
 require 'log-merge/log_combiner'
-
+require 'log-merge/merge_cli'
 
